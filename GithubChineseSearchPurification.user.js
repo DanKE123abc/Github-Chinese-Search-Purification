88a5fcb4099809b0f3d810102625609fc5c31ad7
DanKe <1586017466@QQ.com>
2024-07-06 01:24:53 +0800

    更新 GithubChineseSearchPurification.user.js

--- GithubChineseSearchPurification.user.js
+++ GithubChineseSearchPurification.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         Github中文搜索净化
 // @namespace    http://danke666.top/
-// @version      1.1
+// @version      1.2
 // @author       DanKe
-// @description  Github中文搜索净化，我不想讨论政治，屏蔽特定用户群
+// @description  Github中文搜索净化，屏蔽特定用户群
 // @match        https://github.com/search*
 // @match        https://github.com/*
 // @grant        none
@@ -14,7 +14,7 @@
 
     // 定义要检测和删除的用户名列表
     const usernamesToRemove = [
-        'cirosantilli', //我去你妈了个表Github是你们搞政治的地方吗
+        'cirosantilli',//我去你妈了个表Github是你们搞政治的地方吗
         'tjqJ62cESiHPj6DdR6vXDAcPp',
         'Bellum45',
         'Daravai1234',
@@ -29,18 +29,36 @@
         'panbinibn',
         'zaohmeing',
         'zhaohmng-outlook-com',
-        ];
+    ];
 
+    function removeElementsWithUsername() {
+        const userElements = document.querySelectorAll('.Box-sc-g0xbh4-0');
+        userElements.forEach(element => {
+            const usernameElement = element.querySelector('a[href^="/"]');
+            if (usernameElement) {
+                const username = usernameElement.getAttribute('href').split('/')[1];
+                if (usernamesToRemove.includes(username)) {
+                    element.remove();
+                }
+            }
+        });
+    }
 
-    const userElements = document.querySelectorAll('.Box-sc-g0xbh4-0');
-
-    userElements.forEach(element => {
-        const usernameElement = element.querySelector('a[href^="/"]');
-        if (usernameElement) {
-            const username = usernameElement.getAttribute('href').split('/')[1];
-            if (usernamesToRemove.includes(username)) {
-                element.remove();
+    function observerCallback(mutationsList, observer) {
+        for(const mutation of mutationsList) {
+            if (mutation.type === 'childList') {
+                removeElementsWithUsername();
             }
         }
-    });
+    }
+
+    const observer = new MutationObserver(observerCallback);
+    window.onload = function() {
+        removeElementsWithUsername();
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true
+        });
+    };
+
 })();
