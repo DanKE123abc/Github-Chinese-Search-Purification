3971f33e57722497bf1f82c8d9e4bc2d6c9ccdde
DanKe <1586017466@QQ.com>
2024-07-13 12:23:35 +0800

    Update GithubChineseSearchPurification.user.js

--- GithubChineseSearchPurification.user.js
+++ GithubChineseSearchPurification.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Github中文搜索净化
 // @namespace    http://danke666.top/
-// @version      1.2
+// @version      1.3
 // @author       DanKe
 // @description  Github中文搜索净化，屏蔽特定用户群
 // @match        https://github.com/search*
@@ -14,7 +14,7 @@
 
     // 定义要检测和删除的用户名列表
     const usernamesToRemove = [
-        'cirosantilli',//我去你妈了个表Github是你们搞政治的地方吗
+        'cirosantilli', //我去你妈了个表就是你这私人东西祸害环境
         'tjqJ62cESiHPj6DdR6vXDAcPp',
         'Bellum45',
         'Daravai1234',
@@ -31,17 +31,35 @@
         'zhaohmng-outlook-com',
     ];
 
+    function addBlockedNotice() {
+        const resultsList = document.querySelector('div[data-testid="results-list"]');
+        if (resultsList) {
+            const notice = document.createElement('div');
+            notice.style.cssText = 'text-align: center; padding: 10px; margin: 10px; background-color: #f9f9f9; border: 1px solid #e1e4e8;';
+            notice.textContent = '已屏蔽不受欢迎的仓库';
+            resultsList.appendChild(notice);
+        }
+    }
+
     function removeElementsWithUsername() {
-        const userElements = document.querySelectorAll('.Box-sc-g0xbh4-0');
-        userElements.forEach(element => {
-            const usernameElement = element.querySelector('a[href^="/"]');
-            if (usernameElement) {
-                const username = usernameElement.getAttribute('href').split('/')[1];
-                if (usernamesToRemove.includes(username)) {
-                    element.remove();
+        const resultsList = document.querySelector('div[data-testid="results-list"]');
+        if (resultsList) {
+            const userElements = resultsList.querySelectorAll('.Box-sc-g0xbh4-0');
+            let hasRemoved = false;
+            userElements.forEach(element => {
+                const usernameElement = element.querySelector('a[href^="/"]');
+                if (usernameElement) {
+                    const username = usernameElement.getAttribute('href').split('/')[1];
+                    if (usernamesToRemove.includes(username)) {
+                        element.remove();
+                        hasRemoved = true;
+                    }
                 }
+            });
+            if (hasRemoved) {
+                addBlockedNotice();
             }
-        });
+        }
     }
 
     function observerCallback(mutationsList, observer) {
@@ -55,10 +73,11 @@
     const observer = new MutationObserver(observerCallback);
     window.onload = function() {
         removeElementsWithUsername();
-        observer.observe(document.body, {
+        observer.observe(document.querySelector('div[data-testid="results-list"]'), {
             childList: true,
             subtree: true
         });
     };
+
 
 })();
